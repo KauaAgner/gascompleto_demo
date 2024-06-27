96f065c8c1fe20fb82266f4285047f09dc0d6a4b
KauaAgner <1387119@sga.pucminas.br>
2024-06-27 18:36:16 -0300

    teste

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,6 +5,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
   if (token == null) {
     perfilUsuarioButton.textContent = "Login";
-    perfilUsuarioLink.href = "../html/signin.html";
+    perfilUsuarioLink.href = "/gascompleto_demo/html/signin.html";
   }
 })
